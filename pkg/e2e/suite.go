d0184f8042e327c4f8b0e1407cd3c0b930b24cf1
yasun <yasun@redhat.com>
2026-02-04 17:35:45 +0800

    Fix Junit report issue

--- pkg/e2e/suite.go
+++ pkg/e2e/suite.go
@@ -2,12 +2,8 @@ package e2e
 
 import (
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/onsi/ginkgo/v2"
-	"github.com/onsi/ginkgo/v2/reporters"
-	"github.com/spf13/viper"
 
 	"github.com/openshift-hyperfleet/hyperfleet-e2e/pkg/config"
 	"github.com/openshift-hyperfleet/hyperfleet-e2e/pkg/helper"
@@ -41,38 +37,9 @@ var _ = ginkgo.BeforeSuite(func() {
 	cfg.Display()
 
 	logger.Info("starting hyperfleet-e2e test suite - each test creates temporary resources")
-
-	if junitPath := viper.GetString(config.Tests.JUnitReportPath); junitPath != "" {
-		setupJUnitReporter(junitPath)
-	}
 })
 
 var _ = ginkgo.AfterSuite(func() {
 	helper.ClearSuiteConfig()
 	logger.Info("test suite completed")
 })
-
-// setupJUnitReporter configures JUnit XML report generation
-func setupJUnitReporter(junitPath string) {
-	ginkgo.ReportAfterSuite("HyperFleet E2E JUnit Report", func(report ginkgo.Report) {
-		dir := filepath.Dir(junitPath)
-		if err := os.MkdirAll(dir, 0750); err != nil {
-			logger.Error("failed to create report directory", "path", dir, "error", err)
-			return
-		}
-
-		err := reporters.GenerateJUnitReportWithConfig(
-			report,
-			junitPath,
-			reporters.JunitReportConfig{
-				OmitSpecLabels:   true,
-				OmitLeafNodeType: true,
-			},
-		)
-		if err != nil {
-			logger.Error("failed to create JUnit report", "path", junitPath, "error", err)
-		} else {
-			logger.Info("junit report written", "path", junitPath)
-		}
-	})
-}
